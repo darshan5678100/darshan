c905d69d7c494460f68d4834c07b6adfa5131467
darshan5678100 <64181790+darshan5678100@users.noreply.github.com>
2020-04-23 09:20:20 +0530

    Add files via upload

---
+++ LibraryException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagmentSystem.exception
+{
+   public class LibraryException : Exception 
+    {
+     public LibraryException():base()
+        {
+
+        }
+        public LibraryException(string message):base(message)
+        {
+
+        }
+        public LibraryException(string message,Exception innerException):base(message,innerException)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
